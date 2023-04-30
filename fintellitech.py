b76b7a04aac3e1a7595ccd12ecf0e32757ee29ba
AlexM141200 <alex.manson14@gmail.com>
2023-04-30 12:24:22 +0100

    Changes

--- fintellitech.py
+++ fintellitech.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request, render_template
 import long_responses as long
 import chat as chatbot
 
+# import facereco as face
+
 app = Flask(__name__)
 
 
@@ -11,6 +13,30 @@ def chat():
     message = data['message']
     response = chatbot.check_all_messages(message.split())
     return jsonify({'response': response})
+
+# Initialize the face recognition class
+# dfu = face.Dlib_Face_Unlock()
+
+# Route for the face recognition functionality
+
+
+# @app.route("/face_recognition", methods=['POST'])
+# def face_recognition():
+# data = request.get_json()
+# response = {}
+# Call the ID() method of the Dlib_Face_Unlock class to capture an image of the user
+# and compare it with the images of known users in the system
+# user = dfu.ID()
+# if user:
+#  response['message'] = 'Hello, ' + user[0] + '!'
+# else:
+#  response['message'] = 'Face not recognized'
+# return jsonify(response)
+
+
+@ app.route("/login")
+def login():
+    return render_template('login.html')
 
 
 @ app.route("/")
--- static/app.js
+++ static/app.js
@@ -61,4 +61,3 @@ $(function () {
     }
 });
 
-console.log(xhr.responseText);
